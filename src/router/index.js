3dcfb3efd8fc5855d0a931d8e77b33cb5ce5407e
RIDWAN RAJI <ror716@mail.usask.ca>
2018-03-29 06:10:38 -0600

    Define routing pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Bookings from '@/components/Bookings'
+import Gallery from '@/components/Gallery'
+import About from '@/components/About'
 
 Vue.use(Router)
 
@@ -8,8 +11,26 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'Home',
       component: Home
+    },
+    {
+      path: '/BookSession',
+      name: 'Book A Session',
+      icon: '',
+      component: Bookings
+    },
+    {
+      path: '/Gallery',
+      name: 'Gallery',
+      icon: '',
+      component: Gallery
+    },
+    {
+      path: '/About',
+      name: 'About',
+      icon: '',
+      component: About
     }
   ]
 })
