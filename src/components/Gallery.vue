dfc2798bb2b50bc5d0782036190739df1fad6171
RIDWAN RAJI <ror716@mail.usask.ca>
2018-04-05 22:09:58 -0600

    Make gallery images show in grid form

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,10 +1,11 @@
 <template>
-  <v-container fluid grid-list-md>
-    <v-layout row wrap
-    v-for="image in images"
-    :key="image.src" >
-      <v-flex d-flex xs12 sm6 md5 lg3 xl2 align-end flexbox>
-        <v-card >
+  <v-container fluid fill-height>
+    <v-layout row wrap>
+      <v-flex
+      v-for="image in images"
+      :key="image.src"
+      xs12 sm6 md5 lg3 xl2>
+        <v-card class="my-2 mx-2">
                   <v-card-media
                       class="white--text mb-0"
                       height="400px"
